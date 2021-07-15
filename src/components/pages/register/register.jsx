bdd7623d7b22353edf0baa9dee841eaf84ccadbd
Sam-McCathie <sammccathie2@gmail.com>
2021-07-15 13:34:27 +1200

    Complete register page - no .js

--- src/components/pages/register/register.jsx
+++ src/components/pages/register/register.jsx
@@ -1,11 +1,55 @@
 import React from "react";
+import "./register.css";
 import Header from "../../shared/header/header";
 
 function register() {
   return (
     <div>
       <Header register="current-page" />
-      <span>Register</span>
+      <div className="register-body">
+        <div>
+          <h1 className="register-header">Register</h1>
+          <form>
+            <div className="register-form">
+              <div className="register-input-options">
+                {/*Left side of div */}
+                <label className="register-input-label register-email-label">
+                  Email:
+                </label>
+                <label className="register-input-label register-username-label">
+                  Username:
+                </label>
+                <label className="register-input-label register-password-label">
+                  Password:
+                </label>
+              </div>
+              {/*Right side of div */}
+              <div className="register-inputs">
+                <input
+                  type="email"
+                  className="register-input register-email-input"
+                />
+                <input
+                  type="text"
+                  className="register-input register-username-input"
+                />
+
+                <input
+                  type="text"
+                  className="register-input register-password-input"
+                />
+              </div>
+            </div>
+            <div className="register-button-container">
+              <input
+                type="submit"
+                value="Register"
+                className="register-button"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
