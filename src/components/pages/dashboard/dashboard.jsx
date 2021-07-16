4d4c6d1e28199e84b2fb8cca31fe0067c2fcbe14
Sam-McCathie <sammccathie2@gmail.com>
2021-07-16 16:09:14 +1200

    connect backend to dashboard

--- src/components/pages/dashboard/dashboard.jsx
+++ src/components/pages/dashboard/dashboard.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Axios from "axios";
 import "./dashboard.css";
 import HeaderLoggedIn from "../../shared/header/header-logged-in";
 
 function Dashboard() {
+  const [user, setUser] = useState("");
+  const [flat, setFlat] = useState("");
+
+  //GET username
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/user/current").then((response) => {
+      setUser(response.data[0]);
+    });
+  }, []);
+
+  // GET flat name
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/flat/list").then((response) => {
+      setFlat(response.data[0]);
+    });
+  }, []);
+
   return (
     <div>
       <HeaderLoggedIn dashboard="current-page" />
-      <span>Dashboard</span>
+      <div>
+        <h1>Dashboard</h1>
+        <p>Welcome: {user.username}</p>
+        <p>My flat: {flat.flatName}</p>
+      </div>
     </div>
   );
 }
