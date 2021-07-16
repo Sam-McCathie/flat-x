c50862b19b25be5780ab17d09b3ba7bf32ced7db
Sam-McCathie <sammccathie2@gmail.com>
2021-07-16 13:54:05 +1200

    Logged in header completed

--- src/components/pages/dashboard/dashboard.jsx
+++ src/components/pages/dashboard/dashboard.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "./dashboard.css";
+import HeaderLoggedIn from "../../shared/header/header-logged-in";
 
-function dashboard() {
+function Dashboard() {
   return (
     <div>
+      <HeaderLoggedIn dashboard="current-page" />
       <span>Dashboard</span>
     </div>
   );
 }
 
-export default dashboard;
+export default Dashboard;
--- src/components/shared/header/header-logged-in.jsx
+++ src/components/shared/header/header-logged-in.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import "./header-logged-in.css";
+import {Link} from "react-router-dom";
 
-function headerLoggedIn() {
+//Convert to reusable prop
+
+function headerLoggedIn(props) {
   return (
-    <div>
-      <span>See this header when you sign in</span>
+    <div className="logged-in-header">
+      <div className="logged-in-header-links">
+        <div className="logged-in-link-button">
+          <Link to="/dashboard">
+            <p className={`logged-in-header-link ${props.dashboard}`}>
+              Dashboard
+            </p>
+          </Link>
+        </div>
+        <div className="logged-in-link-button">
+          <Link to="/my-flat">
+            <p className={`logged-in-header-link ${props.flat}`}>My Flat</p>
+          </Link>
+        </div>
+        <div className="logged-in-link-button">
+          <Link to="/my-profile">
+            <p className={`logged-in-header-link ${props.profile}`}>
+              My Profile
+            </p>
+          </Link>
+        </div>
+        <div>
+          <div className="logged-in-link-button">
+            <Link to="/">
+              <p className="logged-in-header-link">Logout</p>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
